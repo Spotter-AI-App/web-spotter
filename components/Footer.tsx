a7a7d38d4d3c8044cb7f791c3ce9a8b35cc84f1d
centenohugo <hcienteno@gmail.com>
2025-12-23 13:58:38 +0100

    Include name business

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,12 +11,13 @@ const Footer = () => {
   return (
     <footer className="bg-white dark:bg-black shadow-sm text-gray-400 py-6">
       <div className="container mx-auto px-4 flex flex-wrap justify-center sm:justify-between items-center text-sm">
-        <p className="ml-4">&copy; {currentYear} Spotter AI. {content.footer.copyright}</p>
         <div className="flex items-center">
           <a href="/">
             <img src="logos/icon_transparent.png" alt="logo" className="w-12 h-12" />
           </a>
         </div>
+        <p className="ml-4">&copy; {currentYear} Spotter AI. {content.footer.copyright}</p>
+        <p className="ml-4">by CAMPUS INFORMATION TECHNOLOGIES S.L.</p>
       </div>
     </footer>
   );
